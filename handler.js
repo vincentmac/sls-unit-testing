e1e7a59aba9e57ee742ac22da273b789b1ced6f4
Vincent Mac <vincent@simplicity.io>
2020-08-07 15:11:29 -0700

    first commit

---
+++ handler.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports.endpoint = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: getLocalGreeting(pickLocale()),
+    }),
+  };
+
+  callback(null, response);
+};
+
+function getLocalGreeting(language) {
+  switch(language) {
+    case "en":
+      return "Hello!";
+    case "es":
+      return "¡Hola!";
+    case "ru":
+      return "Привет!";
+    default:
+      return "👋";
+  }
+}
+module.exports.getLocalGreeting = getLocalGreeting;
+
+
+function pickLocale() {
+  const languages = ["en", "es", "cn", "fr", "ru"];
+	// We miss Python's random.choice
+  return languages [Math.floor(Math.random() * languages.length)];
+}
